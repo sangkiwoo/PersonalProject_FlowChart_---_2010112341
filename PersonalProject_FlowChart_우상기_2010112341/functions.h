1b94dc6341ab4d80338a9a4e537735f2b6168f61
sangkiwoo <sangd1108@gmail.com>
2015-10-03 11:08:10 +0900

    Error Commiting

---
+++ PersonalProject_FlowChart_우상기_2010112341/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_F
+#define FUNCTION_F
+
+void Menulist();	// function for displaying menu
+void Size();		// function for beverage size	
+void Option();		// function for beverage option
+void Option_Mag();	// function for the amount of beverage
+
+#endif
